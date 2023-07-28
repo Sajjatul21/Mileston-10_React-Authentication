754d8cb78d96f6629218fcb5ad6106a6ae3b4e82
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-28 04:23:54 -0700

    firebase create and install firebase

---
+++ 56 Simple React Firebase authentication/simple-firebase-authentication/src/firebase/firebase.inti.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDMQDDvpAsL7M0g7poy5mRc1GV0DNfqli4",
+    authDomain: "simple-firebase-authenti-a23b4.firebaseapp.com",
+    projectId: "simple-firebase-authenti-a23b4",
+    storageBucket: "simple-firebase-authenti-a23b4.appspot.com",
+    messagingSenderId: "130736981196",
+    appId: "1:130736981196:web:fd2754cc55a3d211ecee69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
