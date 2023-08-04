cf8e2b4641fd567efae51f1cc771f01c7b7764b2
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-04 03:29:16 -0700

    setup firebase  and setup  react router path on main.jsx

---
+++ 56 Simple React Firebase authentication/simple-firebase/src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+    * 1. visit console.firebase.google.com
+    * 2. crete project (skip google analytics)
+    * 3. register app( create config)
+    * 4. install firebase: npm install firebase 
+    * 5. add config file t your projects
+    * 6 .DANGER: Do not publish or make firebase config to public by pushing those to github.
+*/
---
+++ 56 Simple React Firebase authentication/simple-firebase/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqn3dbfufonVifUD7w1-MdlqgiFhWpDAc",
+  authDomain: "simple-firebase-a58ec.firebaseapp.com",
+  projectId: "simple-firebase-a58ec",
+  storageBucket: "simple-firebase-a58ec.appspot.com",
+  messagingSenderId: "104088432126",
+  appId: "1:104088432126:web:e842a44cceb53f2ed4a4af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
