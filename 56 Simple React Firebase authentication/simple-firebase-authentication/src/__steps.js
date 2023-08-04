114825b2c9093cdbca49ca88344cdedd61d9b9d2
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-04 07:23:50 -0700

    create login component and link

--- 56 Simple React Firebase authentication/simple-firebase-authentication/src/__steps.js
+++
@@ -1,9 +0,0 @@
-/* 
-    1. visit: console.firebase.google.com
-    2. crete a new firebase projects
-    3. visit doc (got to docs) > Build > Authentication > web > Getting started 
-    4. Register web app > firebase project home > click web > give name and register
-    5. Install firebase for your projects: npm install firebase
-    6. Dangerous: get firebase config and put it in firebase.init.js
-    7. export app
-*/
