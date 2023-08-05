cca4d8975515329fd2e66dcb25662408eda0ffc1
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-04 23:47:04 -0700

    add event handler  to use google access  within the event handler .
    display google user

--- 56 Simple React Firebase authentication/dimple-firebase/src/App.jsx
+++ 56 Simple React Firebase authentication/dimple-firebase/src/App.jsx
@@ -1,19 +1,38 @@
 import './App.css';
-import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import app from './firebase/firebase.config';
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
   return (
     <div>
 
       <h1>FireBase + React</h1>
-      <div className="card">
-        <button>Google Sign IN</button>
-      </div>
 
+      <button onClick={handleGoogleSingIn}>Google SignIn</button>
+
+      {user && <div className='card'>
+        <h3>User: {user.displayName}</h3>
+        <p>Email: {user.email}</p>
+      </div>
+      }
     </div>
   );
 }
