f9c8bc0ae289774064c388d81df492db90f04249
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-04 23:33:22 -0700

    set getAuth and googleAuthProvider

--- 56 Simple React Firebase authentication/dimple-firebase/src/App.jsx
+++ 56 Simple React Firebase authentication/dimple-firebase/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import app from './firebase/firebase.config';
 
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 function App() {
-
   return (
     <div>
 
       <h1>FireBase + React</h1>
-      <div className="card">  
-
+      <div className="card">
+        <button>Google Sign IN</button>
       </div>
 
     </div>
--- 56 Simple React Firebase authentication/dimple-firebase/src/firebase/firebase.config.js
+++ 56 Simple React Firebase authentication/dimple-firebase/src/firebase/firebase.config.js
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFKtyvvhMlY-4VOus3F98tAIahsFfyESY",
-  authDomain: "dimple-firebase-9c0f4.firebaseapp.com",
-  projectId: "dimple-firebase-9c0f4",
-  storageBucket: "dimple-firebase-9c0f4.appspot.com",
-  messagingSenderId: "650163202887",
-  appId: "1:650163202887:web:14ca0f4419cdfacb0c3a62"
+    apiKey: "AIzaSyDFKtyvvhMlY-4VOus3F98tAIahsFfyESY",
+    authDomain: "dimple-firebase-9c0f4.firebaseapp.com",
+    projectId: "dimple-firebase-9c0f4",
+    storageBucket: "dimple-firebase-9c0f4.appspot.com",
+    messagingSenderId: "650163202887",
+    appId: "1:650163202887:web:14ca0f4419cdfacb0c3a62"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
