b74097a13f4146e2927bcb7d570044dda0fca697
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-04 23:26:41 -0700

    project set  up and firebase install

---
+++ 56 Simple React Firebase authentication/dimple-firebase/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>Vite + React</h1>
+      <div className="card">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
