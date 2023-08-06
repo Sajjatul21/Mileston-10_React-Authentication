e0ac82f0364d836a4c455b29b20f1ef470c565b2
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-05 20:29:10 -0700

    create component  set route
    install react router dom  and firebase

---
+++ 57 Email Password Authentication, Login From/1-email-password-auth/src/components/__steps__.js
@@ -0,0 +1,9 @@
+/* 
+    * 1. authentication
+    * 2. authorization
+    * 3. sign in === login  // আমাদের আগে থেকে sign in / login করা আছে
+    * 4. sign up === register // যদি ইউজার  নতুন হয় তাহলে সে sign up / register করবে 
+       
+      
+
+*/
