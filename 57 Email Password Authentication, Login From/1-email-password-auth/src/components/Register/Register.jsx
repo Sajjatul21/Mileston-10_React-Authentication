8d49d0b5327c85c6c029545e75dc57b820aa8aa7
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-05 21:12:27 -0700

    আমরা onChange and onBlur এর মাধ্যমে input field থেকে value  নিতে পারি

--- 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Register/Register.jsx
+++ 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Register/Register.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const handleEmailChange = (event) => {
+        // console.log(event);
+        console.log(event.target.value);
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordBlur = event => {
+        console.log(event.target.value);
+    };
     return (
         <div>
             <h2>Please Register</h2>
             <form >
-                <input type="email" name="email" id="email" placeholder='Your email' />
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your email' />
                 <br />
-                <input type="password" name="password" id="password" placeholder='Your password' />
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your password' />
                 <br />
                 <input type="submit" value="Register" />
             </form>
