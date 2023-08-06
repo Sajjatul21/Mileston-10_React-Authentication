aee39148afe86ba8e8e2877ce7b067a9c8057254
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-05 23:06:01 -0700

    set onSubmit and handler

--- 57 Email Password Authentication, Login From/1-email-password-auth/src/components/RegisterRBS/RegisterRBS.jsx
+++ 57 Email Password Authentication, Login From/1-email-password-auth/src/components/RegisterRBS/RegisterRBS.jsx
@@ -4,18 +4,26 @@ import Form from 'react-bootstrap/Form';
 
 
 const RegisterRBS = () => {
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+    };
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Register</h2>
-            <Form>
+            <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Accept Terms and Conditions" />
