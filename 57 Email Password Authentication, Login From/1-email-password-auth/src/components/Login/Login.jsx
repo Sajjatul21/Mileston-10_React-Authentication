3d137e971279a8e47a46fb693a6180a1c466cbef
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-06 09:04:02 -0700

    password validation regular expression
    https://stackoverflow.com/questions/5142103/regex-to-validate-password-strength

--- 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Login/Login.jsx
+++ 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Login/Login.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
     const handleLogin = event => {
         event.preventDefault();
         const from = event.target;
         const email = from.email.value;
         const password = from.password.value;
         console.log(email, password);
+
+        //password validation
+        setError('');     // আমরা যদি এখানে empty string না দেই তখন আমরা যখন password validation এর বাহিরে password দিব তখন  তো আমাদরে error দেখবে তারপর আমরা যখস তা ঠিক করবো তখন সে error টাকে state এ রেখে দিবে তখন আমরা correct password দিলেও সে error যাবে না তাই যখন error না পাবে তখন  আমরা state টা empty string করে দিব।
+        setSuccess('');
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least two uppercase');
+            return;
+        }
+        else if (!/(?=.*[!@#$%^&*])/.test(password)) {
+            setError('Please add a special character.');
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 characters');
+        }
 
     };
 
@@ -29,6 +47,7 @@ const Login = () => {
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
+            <p className='text-danger'>{error}</p>
         </div>
     );
 };
