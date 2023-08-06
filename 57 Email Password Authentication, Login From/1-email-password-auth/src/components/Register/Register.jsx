ccd34c0397ed311302f4110627de405a9a2687e7
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-08-05 20:36:47 -0700

    create form

--- 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Register/Register.jsx
+++ 57 Email Password Authentication, Login From/1-email-password-auth/src/components/Register/Register.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <h4>Register</h4>
+            <h2>Please Register</h2>
+            <form >
+                <input type="email" name="email" id="email" placeholder='Your email' />
+                <br />
+                <input type="password" name="password" id="password" placeholder='Your password' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
